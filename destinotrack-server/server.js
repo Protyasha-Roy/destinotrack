6cf2a0d88f055177012ce7afa08ec09d72d637c7
Protyasha roy <protyasharoy369@gmail.com>
2024-01-22 18:09:19 +0600

    added group to check roll

--- destinotrack-server/server.js
+++ destinotrack-server/server.js
@@ -100,7 +100,7 @@ app.post('/login', async (req, res) => {
 });
 
 app.post('/submitForm', async (req, res) => {
-  const { roll, sessionYear } = req.body;
+  const { roll, sessionYear, group } = req.body;
 
   try {
     // Check if student with the same roll and session year already exists
